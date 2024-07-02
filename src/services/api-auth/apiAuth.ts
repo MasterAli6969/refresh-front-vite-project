717627215e0080e0b22040919449a731ec26c17e
MasterAli6969 <mars.mir6969@gmail.com>
2024-07-02 15:44:38 +0300

    начала интеграции с бэкендом страницы автаризации

--- src/services/api-auth/apiAuth.ts
+++ src/services/api-auth/apiAuth.ts
@@ -5,9 +5,19 @@ import { AuthPostDataTypes } from "../../components/auth-components/authorizatio
 export const postAuthorizationData = async (params: AuthPostDataTypes) => {
   const authPostDataJson = JSON.stringify(params);
   console.log("ДАННЫЕ ПОСЛЕ ПАРСА", authPostDataJson);
-  const response = await axios.post(
-    "http://45.135.165.89:8082/",
-    authPostDataJson
-  );
+  const response = await axios.post("http://45.135.165.89:8082/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log("ОТВЕТ СЕРВЕРА", response);
+
   return response.data;
 };
+
+// export const gogHead = {
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "http://localhost:5173/",
+//   },
+// };
