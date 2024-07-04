13d6bd98eff99ed3fd0ee4e5f97d053457f531c8
MasterAli6969 <mars.mir6969@gmail.com>
2024-07-04 17:11:27 +0300

    начала интеграции с бэкендом страницы автаризации

--- src/pages/central-panel/ControlPanel.tsx
+++ src/pages/central-panel/ControlPanel.tsx
@@ -1,19 +1,25 @@
 import { FC, useRef } from "react";
-import { Grid } from "@mui/material";
 
 import styles from "./central_panel.module.scss";
 import CentralPanelPcIcons from "../../components/central-header/central-panel-components/central-panel-pc-icons/CentralPanelPcIcons";
+import CentralHeader from "../../components/central-header/CentralHeader";
+import RightSidebar from "../../components/right-sidebar/RightSidebar";
 
 const ControlPanel: FC = () => {
   const panelRef = useRef(null);
 
   return (
-    <>
-      <div className={styles.div} ref={panelRef}>
-        <CentralPanelPcIcons />
+    <div className="m-0 p-0 row">
+      <div className="m-0 col-md-9">
+        <CentralHeader />
+        <div className={styles.div} ref={panelRef}>
+          <CentralPanelPcIcons />
+        </div>
       </div>
-      ;
-    </>
+      <div className="m-0 p-0 col-md-3">
+        <RightSidebar />
+      </div>
+    </div>
   );
 };
 
